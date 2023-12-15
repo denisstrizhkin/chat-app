31d6a5c7da4ad7c4264c666b28cb354b2494ac59
Denis Strizhkin <strdenis02@gmail.com>
2023-12-15 10:40:30 +0300

    added all tables and columns

--- backend/constants.js
+++ backend/constants.js
@@ -1,5 +1,18 @@
 export const TABLE_USERS = 'users';
+export const TABLE_CHANNELS = 'channels';
+export const TABLE_CHANNEL_USERS = 'channel_users';
+export const TABLE_MESSAGES = 'messages';
 
+export const USER_ID = 'id';
 export const USER_NAME = 'name';
 export const USER_PASSWORD = 'password';
-export const USER_ID = 'id';
+
+export const CHANNEL_ID = 'id';
+export const CHANNEL_NAME = 'name';
+export const CHANNEL_CREATOR = 'created_by';
+
+export const MESSAGE_ID = 'id';
+export const MESSAGE_NAME = 'name';
+export const MESSAGE_CHANNEL = 'channel';
+export const MESSAGE_AUTHOR = 'sent_by';
+export const MESSAGE_DATE = 'date';
