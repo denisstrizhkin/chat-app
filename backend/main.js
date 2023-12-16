974f744b24bc16909790ee960aabe7afe3e921bf
Denis Strizhkin <strdenis02@gmail.com>
2023-12-16 03:40:21 +0300

    use cors

--- backend/main.js
+++ backend/main.js
@@ -21,6 +21,7 @@ const port = process.env.PORT || "3000";
 app.set("port", port);
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
